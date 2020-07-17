df584cb2851fa784629009505187bfb56daeb812
Fabien Mairesse <fabien.mairesse@docdoku.com>
2020-07-17 12:15:12 +0200

    Mise en place Espresso

--- app/src/androidTest/java/com/example/renaultrx/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/renaultrx/ExampleInstrumentedTest.kt
@@ -2,11 +2,13 @@ package com.example.renaultrx
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +17,18 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule
+    var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.renaultrx", appContext.packageName)
+    }
+
+    @Test
+    fun testCommunes() {
+        Thread.sleep(10000)
     }
 }
